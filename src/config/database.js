f01a228faced64aad435c16d206ba1e9556d0edb
Jovennan Ramalho <jovennan@gmail.com>
2020-01-21 15:47:40 -0300

    parei na migration de user

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'jovennan',
+    password: '123456',
+    port: '5432',
+    database: 'sqlnode',
+    define: {
+        timestamp: true,
+        underscored: true
+    }
+};
